b1edc17df32712b1d99e40c80770895015a32d94
Sithum kavinda <it20050344@my.sliit.lk>
2021-10-12 22:04:14 +0530

    Spinner added to the converter page

--- app/src/main/java/com/edu/calarea/convert_page.java
+++ app/src/main/java/com/edu/calarea/convert_page.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class convert_page extends AppCompatActivity {
+public class convert_page extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     ImageView btnBack;
+    Spinner spinner_metric;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,7 @@ public class convert_page extends AppCompatActivity {
 
         //Bind with id
         btnBack = findViewById(R.id.btn_back);
+        spinner_metric = findViewById(R.id.spinnerMetric);
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +30,24 @@ public class convert_page extends AppCompatActivity {
                 finish();
             }
         });
+
+        //Set Items to Spinner(Drop down)
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.metric, R.layout.support_simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner_metric.setAdapter(adapter);
+
+        //Get the selected item from the spinner
+        spinner_metric.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String choice = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(this, "Item: " + choice, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
